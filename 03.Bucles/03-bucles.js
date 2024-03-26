3643fec24a6f689a3d1def837497e58f6d0b1505
Your Name <you@example.com>
2024-03-26 15:01:11 -0300

    trabajo heecho

--- 03.Bucles/03-bucles.js
+++ 03.Bucles/03-bucles.js
@@ -1,52 +1,93 @@
-function cantidadDeVocales(phrase){
-// Cuenta la cantidad de vocales en la frase pasada por parametros utilizando un bucle for
-// Tu código:👇
+function cantidadDeVocales(phrase) {
+    // Cuenta la cantidad de vocales en la frase pasada por parametros utilizando un bucle for
+    // Tu código:👇
+    phrase =  phrase.toLowerCase();
+    
+    var contadorVocales = 0;
+    
+    
+    var vocales = ['a', 'e', 'i', 'o', 'u'];
+    
+    
+    
+    for (let i = 0; i < phrase.length; i++) {
+        if (vocales.includes(phrase[i])) {
+            contadorVocales++;
+        }
+    }
+    
+    
+    return contadorVocales;
 
 }
 
 function primerDivisible(divisor, dividendo) {
-// Encuentra el primer número divisible por 'dividendo' a partir de un número dado (divisor) utilizando un bucle while
-// Tu código:👇
+    // Encuentra el primer número divisible por 'dividendo' a partir de un número dado (divisor) utilizando un bucle while
+    // Tu código:👇
+    vali=true
+    while(vali){
+        if ((dividendo%divisor)==0){
+            break
+        }
+            divisor++
+
+    }
+    return divisor
 
 }
 
 
-function encuentraX(x){
-//Realiza un algoritmo que recorre numeros del 1 al 100, cuando pase por el numero X pasado por parametros
-// retornar "El numero X es: ..."
-// Tu Código:👇
+function encuentraX(x) {
+    //Realiza un algoritmo que recorre numeros del 1 al 100, cuando pase por el numero X pasado por parametros
+    // retornar "El numero X es: ..."
+    // Tu Código:👇
+    for (let i = 0; i <=100; i++) {
+        if (x==i){
+            return "El numero X es: "+ x
+        }
+        
+    }
 
 }
 
-function sumandoTodo(num){
-// Realiza un algoritmo que el cual recorra del 0 al numero pasado por parametros, debes retornar la suma de todo los
-// numeros que se recorrio
-// Tu código:👇
-
+function sumandoTodo(num) {
+    // Realiza un algoritmo que el cual recorra del 0 al numero pasado por parametros, debes retornar la suma de todo los
+    // numeros que se recorrio
+    // Tu código:👇
+    contador=0;
+    for (let i = 0; i <=num; i++) {
+        contador=contador+i
+        
+    }
+    return contador
 }
 
 function BinarioADecimal(num) {
-// La funcion recibe por parametro, un numero binario pero en tipo de dato String
-// debes tomar este numero binario y ayudandote de un bucle pasarlo a binario
-// tu código:👇
+    // La funcion recibe por parametro, un numero binario pero en tipo de dato String
+    // debes tomar este numero binario y ayudandote de un bucle pasarlo a binario
+    // tu código:👇
+    for (let i = 0; i <= num.length; i++) {
+        const element = array[i];
+        
+    }
 
 }
 
 function esPrimo(numero) {
-//Escribe un programa que verifique si un número dado es primo o no.
-//Un número primo es aquel que solo es divisible por 1 y por sí mismo.
-//Si el numero pasado es primo devolve: numero " es primo." , caso contrario: numero " no es primo."
-// Tu código:👇
+    //Escribe un programa que verifique si un número dado es primo o no.
+    //Un número primo es aquel que solo es divisible por 1 y por sí mismo.
+    //Si el numero pasado es primo devolve: numero " es primo." , caso contrario: numero " no es primo."
+    // Tu código:👇
 
 }
 
 function invertirCadena(cadena) {
-//La función toma una cadena de texto como parámetro, devuelve la cadena invertida.
-//Ejemplo: 'Hola' -> devolver 'aloH'
-//Tu codigo:👇
+    //La función toma una cadena de texto como parámetro, devuelve la cadena invertida.
+    //Ejemplo: 'Hola' -> devolver 'aloH'
+    //Tu codigo:👇
 
 }
-  
+
 module.exports = {
     encuentraX,
     sumandoTodo,
---
+++ 04.Funciones/04-funciones.js
@@ -0,0 +1,100 @@
+/*  ⚠️⚠️ Ejercicios de funciones - es IMPORTANTE que la funcion se llame como se pide, esto hará que pueda correr los test. ⚠️⚠️
+    ⚠️⚠️ No es necesario que se ejecute a la funcion. ya se ejecuta desde los test. ⚠️⚠️    */
+
+const { contains } = require("micromatch")
+
+//Debes crear un funcion que retorne un mensaje de bienvenida con nombre y apellido de la persona
+//Ejemplo "Bienvenido Leonardo Avila" -> IMPORTANTE la funcion tiene que llamarse 'bienvenida'
+//Tu código:
+const bienvenida =(nombre, apellido)=> `Bienvenido ${nombre} ${apellido}`
+
+
+/**********************************************************************************/
+// Crea una funcion el cual cumpla la funcion de una calculadora.. tiene que recibir 3 parametros num1, operador y num2
+// IMPORTANTE -> la funcion debe llamarse 'calc'
+// Tu código:
+
+    const calc=(num1,operador,num2)=>{
+        if (operador=="-"){
+            return num1 - num2
+        }else if(operador=="+"){
+            return num1+num2
+        }else if (operador=="*"){
+            return num1*num2
+        }
+        else{
+            return num1/num2
+        }
+    }
+
+
+
+
+/**********************************************************************************/
+// Crea una funcion que en base a la temperaturma maxima, min y porcentaje de lluvia, diga si es necesario salir con abrigo,
+// paraguas o sin nada. ejemplos
+// Para max < 15 devolver 'Es recomendable salir con abrigo'
+// Para max < 26 y min > 17 devolver 'El día está lindo'
+// Para min > 25 'El día estará caluroso' 
+// Y para cada uno de los anteriores casos, si el porcentaje de lluvia es mayor a 50 agregar al mensaje 'recomendable un paragua'
+// ejemplo => max = 20 y porcentaje de lluvia = 70 => devolver "El día está lindo, recomendable un paraguas"
+// IMPORTANTE -> la funcion debe llamarse 'temp'    
+// Tu código:
+
+const temp=(max , min , porcentaje)=>{
+    if (max <15){
+        if(porcentaje>50){
+            return 'Es recomendable salir con abrigo, recomendable un paraguas'
+        }else{
+            return 'Es recomendable salir con abrigo'
+        }
+    }else if(max<26 && min>17){
+        if(porcentaje>50){
+            return 'El día está lindo, recomendable un paraguas'
+        }else{
+            return 'El día está lindo'
+        }
+        
+    }else{
+        if(porcentaje>50){
+            return 'El día estará caluroso, recomendable un paraguas'
+        }else{
+            return 'El día estará caluroso'
+        }
+        
+    }
+}
+
+/**********************************************************************************/
+
+// Crea una función que reciba una contraseña, esta función tiene que determinar si contiene al menos 8 caracteres la contraseña.
+// Si la contraseña contiene 8 caracteres, devolver "La contraseña es segura", en caso contrario devolver "La contraseña es insegura"
+// IMPORTANTE -> la funcion debe llamarse 'checkPassword'
+// Tu código:
+
+const checkPassword=(contra)=>contra.length >=8 ? "La contraseña es segura" : "La contraseña es insegura" 
+
+
+/**********************************************************************************/
+// Crea una función que reciba un string como parámetro y devuelva el mismo string pero con todas las palabras en orden inverso.
+// IMPORTANTE -> la funcion debe llamarse 'stringInverso'
+// Tu código:
+const stringInverso=(palabra)=>{
+    let nuevaPalabra=""
+    for (let i = palabra.length-1; i >=0 ; i--) {
+        nuevaPalabra +=palabra[i]
+    }
+    return nuevaPalabra
+}
+
+
+
+/**********************************************************************************/
+
+module.exports = {
+    bienvenida,
+    calc,
+    temp,
+    checkPassword,
+    stringInverso,
+}
